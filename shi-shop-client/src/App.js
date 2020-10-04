fa15233de3b066fd5c8746d4e0e79714a238da4f
unknown <yaweiwei3@gmail.com>
2020-10-04 10:25:23 -0400

    use antd

--- shi-shop-client/src/App.js
+++ shi-shop-client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './shi.png';
 import './App.css';
+import {Button} from 'antd-mobile'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <Button type='primary'>学习</Button>
     </div>
   );
 }
