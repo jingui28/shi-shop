e3a57ef2b7941c6fe1d4dbc05bb4a8ddc66a0ead
unknown <yaweiwei3@gmail.com>
2020-10-06 21:28:58 -0400

    client register login api

---
+++ shi-chat-client/src/api/ajax.js
@@ -0,0 +1,23 @@
+/*
+使用axios 封装的ajax 请求函数
+函数返回的是promise 对象
+*/
+import axios from 'axios'
+
+export default function ajax (url='', data={}, type='GET') {
+    if (type==='GET') {
+        // 准备url query 参数数据
+        let dataStr = '' //数据拼接字符串
+        Object.keys(data).forEach(key => {
+            dataStr += key + '=' + data[key] + "&"
+        })
+        if (dataStr) {
+            dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'))
+            url = url + '?' + dataStr
+        }
+        // 发送get 请求
+        return axios.get(url)
+    } else {
+        return axios.post(url, data)
+    }
+}
---
+++ shi-chat-client/src/api/index.js
@@ -0,0 +1,8 @@
+/*
+包含n 个接口请求函数的模块
+每个函数返回的都是promise 对象
+*/
+import ajax from './ajax'
+
+export const reqLogin = (user) => ajax('/login', user, 'POST')     // 请求登陆
+export const reqRegister = (user) => ajax('/register', user, 'POST')       // 请求注册
