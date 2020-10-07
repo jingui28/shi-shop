565a4262d3d03f24b592bfcb909d09d1e86cb277
unknown <yaweiwei3@gmail.com>
2020-10-06 20:54:33 -0400

    register login API

--- shi-chat-server/routes/index.js
+++ shi-chat-server/routes/index.js
@@ -25,6 +25,7 @@ router.post('/register', function(req, res) {
       // 2.2. 如果不存在, 将提交的user 保存到数据库
       new UserModel({username, type, password: md5(password)}).save(function(err, user) {
         // 生成一个cookie(userid: user._id), 并交给浏览器保存
+        console.log(user, '---------', err, '*******');
         res.cookie('userid', user._id, {maxAge: 1000*60*60*24*7})   // 持久化cookie, 浏览器会保存在本地文件
         // 3.2. 保存成功, 返回成功的响应数据: user
         res.send({code: 0, data: {_id: user._id, username, type}})   // 返回的数据中不要携带pwd
